0a88fe764b946baed4fa937467eef9f75f68ddde
Replit user <>
2023-11-22 12:48:18 +0000

    aggiunto destructor a lez4

--- lez4.py
+++ lez4.py
@@ -2,6 +2,8 @@ class CSVFile():
     def __init__(self,name):
         self.name=name
         self.file=open(self.name,"r")
+    def __del__(self):
+        self.file.close()
     def get_data(self):
         self.file.readline()
         a = self.file.readlines()
@@ -11,5 +13,6 @@ class CSVFile():
             d = c.split(',')
             b.append(d)
         return b
+    
 #csv=CSVFile('shampoo_sales.csv')
 #print(csv.get_data())
